5294c87d5af0ce924a890a33355771fafb0de13c
FraBalFer <franballesterofernandez@gmail.com>
2018-07-31 19:56:13 +0200

    Web services tareas json y xml

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,12 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class DefaultController extends Controller
 {
@@ -12,10 +17,10 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
       * @Route("/tareas", name="tareas")
      */
-    public function tareasAction(Request $request)
+    public function tareasAction()
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:Tarea');
-        $tareas = $repository->findAll();
+        $tareas = $repository->findAllOrderedByDescripcion();
         return $this->render('default/pantalla_tareas.html.twig',
             array('tareas' => $tareas,
                 'mensaje' =>"Hola")
@@ -39,5 +44,48 @@ class DefaultController extends Controller
     );
   }
 
+
+    /**
+      * @Route("/tareas.json", name="tareas_json")
+     */
+    public function tareaJsonTareaAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Tarea');
+        $tareas = $repository->findAllOrderedByDescripcion();
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $json_contenido = $serializer->serialize($tareas, 'json');
+        
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/json');
+        $response->setContent($json_contenido);
+        return $response;
+    }
+
+
+     /**
+      * @Route("/tareas.xml", name="tareas_xml")
+     */
+    public function tareaXmlTareaAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Tarea');
+        $tareas = $repository->findAllOrderedByDescripcion();
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $json_contenido = $serializer->serialize($tareas, 'xml');
+        
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/xml');
+        $response->setContent($json_contenido);
+        return $response;
+    }
 
 }
---
+++ src/AppBundle/Repository/TareaRepository.php
@@ -0,0 +1,17 @@
+<?php
+// src/AppBundle/Repository/TareaRepository.php
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class TareaRepository extends EntityRepository
+{
+    public function findAllOrderedByDescripcion()
+    {
+    	$entityManager = $this->getEntityManager();
+
+    	$query = $entityManager->createQuery('SELECT t FROM AppBundle:Tarea t ORDER BY t.descripcion DESC');
+        
+        return $query->getResult();
+    }
+}
